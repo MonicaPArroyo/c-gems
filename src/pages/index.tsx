fd37a6752c41fe9050c424e5b87ae846d9c81d41
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-04-25 18:59:30 -0600

    Static Props para el Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,17 @@ import Layout from '@components/Layout/Layout';
 import GemHeader from '@components/GemHeader/GemHeader';
 import GemList from '@components/GemList/GemList';
 
-const HomePage = () => {
-	const [gemList, setgemList] = useState<TGem[]>([]);
+export const getStaticProps = async () => {
+	const response = await fetch(`${process.env.API_HOST}/api/gems`);
+	const { data: gemList }: TAPIGemResponse = await response.json();
 
-	useEffect(() => {
-		window
-			.fetch('/api/gems')
-			.then((response) => response.json())
-			.then(({ data, length }) => {
-				setgemList(data);
-			});
-	}, []);
+	return {
+		props: {
+			gemList,
+		},
+	};
+};
+const HomePage = ({ gemList }: { gemList: TGem[] }) => {
 	return (
 		<Layout>
 			<GemHeader />
